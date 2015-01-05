84981b7937e8f2d39f45073a3d80cda4022eb5d8
Hamster Tian <haotia@gmail.com>
2015-01-05 23:04:24 +0800

    blue-common: specify separate fstab file for TWRP
    Files in standard Android format will not be accepted by TWRP
    and will lead to loads of errors in log.
    
    Change-Id: I606c6812855caca6b4adb5b4f9c86a6474aa8ed2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -152,6 +152,7 @@ TW_INCLUDE_FUSE_EXFAT := true
 TW_MAX_BRIGHTNESS := 255
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight_1/brightness"
 TW_SECONDARY_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight_2/brightness"
+TARGET_RECOVERY_FSTAB = device/sony/blue-common/rootdir/twrp.fstab
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
